47848ce3dab526f5405d27b579ec1e7ad684d728
tanvi242 <tanvi.b1724@gmail.com>
2025-08-05 18:17:30 +0530

    Added routing for Admin and User Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,18 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Listings from './pages/Listings';
+import AdminDashboard from './components/AdminDashboard';
+import UserDashboard from './components/UserDashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Listings />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/user" element={<UserDashboard />} />
       </Routes>
     </Router>
   );
